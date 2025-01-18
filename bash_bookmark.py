bd1f18b4087e51330a135eab292d5d95d9980064
Vladimir Puzic <vladimir.puzic@gmail.com>
2025-01-18 19:40:23 +0100

    feat: add save_bookmark() for file creation and bookmark path writing. add return_path() for path search by bookmark name and writing result to file. edited bash_bookmarks.sh, add capability to read result.txt and execute cd to returned path

--- bash_bookmark.py
+++ bash_bookmark.py
@@ -1,23 +1,41 @@
 import sys
+import os
 
 # used during writing process to confirm arguments were passed by bash script
-for i, item in enumerate(sys.argv):
-    print(str(i)+item)
+# for i, item in enumerate(sys.argv):
+#     print(str(i)+item)
 
 # three arguments are passed by the bash script
 # 1 the path of the python script that is called. can be ignored
 # 2 the name of the bookmarked directory. if a new bookmark is being created this arugment is not passes by bash script
 # 3 the current working directory
 
+script_path = os.path.join(os.path.expanduser('~'), 'bash', 'bash_bookmarks')
 
-if len(sys.argv) <= 2: 
+def save_bookmark(path):
     bookmark_name = input('enter bookmark name: ') # used to name a new bookmark upon creation
     
-    with open('bookmarks.csv', 'a') as file: # .csv file storing existing bookmarks
-        print (f'{bookmark_name},{sys.argv[1]}') # used to check values written to bookmarks.csv. not esential
-        file.write(f'{bookmark_name},{sys.argv[1]}\n') # bookmark name and path written to .csv
+    with open((f'{script_path}/bookmarks.csv'), 'a') as file: # .csv file storing existing bookmarks
+#        print (f'{bookmark_name},{path}') # used to check values written to bookmarks.csv. not esential
+        file.write(f'{bookmark_name},{path}\n') # bookmark name and path written to .csv
     print (f'bookmark {bookmark_name} created') # info message
     sys.exit() # SystemExit raised
-else:
-    pass
 
+def return_path(bookmark, path):
+    with open((f'{script_path}/bookmarks.csv'), 'r') as file:
+        for item in file:
+            if bookmark in item:
+                with open((f'{script_path}/result.txt'), 'w') as path_file:
+                    path_file.write((item.split(','))[1])
+                sys.exit()
+            else:
+                save_bookmark()
+            
+if __name__ == '__main__':
+
+    if len(sys.argv) <= 2: 
+        save_bookmark(sys.argv[1])
+
+    else:
+        return_path(sys.argv[1], sys.argv[2])
+        
--- bash_bookmark.sh
+++ bash_bookmark.sh
@@ -2,3 +2,6 @@
 
 PWD=command pwd
 python3 ~/bash/bash_bookmarks/bash_bookmark.py $1 $PWD
+BOOKMARK_PATH=$(cat ~/bash/bash_bookmarks/result.txt)
+
+cd "$BOOKMARK_PATH"
